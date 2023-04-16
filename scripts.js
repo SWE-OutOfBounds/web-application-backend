db38c2ffbe2b855ae28851533716e77c0daf5a49
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-04-16 18:51:14 +0200

    MAJOR ADDITION: 1. passwordValidator : function that check for format 2. checkSecretKey : middleware for checking apps secret keys 3. captchaValidator : TODO middleware that validate clockCAPTCHA input.
    Co-authored-by: SenpaiDodo (live share)

---
+++ scripts.js
@@ -0,0 +1,32 @@
+/**
+ * Controlla se una chiave segreta (secret key) è presente nel database MySQL.
+ *
+ * @param {*} secret - Chiave segreta da controllare
+ */
+export function passwordValidator(string){
+    const regex = /^(?=.?[A-Z])(?=.?[a-z]).{8,}$/i;
+    return regex.test(string);
+}
+export function checkSecretKey(req, res, next) {
+    const secretKey = req.headers['x-secret-key']; // La secret key è passata nell'header della richiesta HTTP
+  
+    // Eseguo una query per verificare se la secret key è presente nel database
+    pool.query('SELECT * FROM Applications WHERE key = ?', [secretKey], (error, results, fields) => {
+      if (error) {
+        // Gestione dell'errore di connessione al database
+        return res.status(500).send("Errore interno al server.");
+      }
+  
+      if (results.length === 0) {
+        // La secret key non è presente nel database
+        return res.status(403).send("Non autorizzato.");
+      }
+  
+      // La secret key è presente nel database, passo al middleware successivo
+      next();
+    });
+  }
+  export function captchaValidator(req, res, next){
+    //TODO
+    next();
+  }
