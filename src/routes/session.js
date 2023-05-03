c402800b86ef5340e75bd8b9ea6049c9fa925def
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-05-03 18:55:57 +0200

    FIX : typo

--- src/routes/session.js
+++ src/routes/session.js
@@ -6,6 +6,5 @@ const clockCaptchaMiddleware = require('../middlewares/clockCaptcha.middleware')
 
 router.route('/').get(clockCaptchaMiddleware.validate,session_controller.recovery)
 router.route('/').post(clockCaptchaMiddleware.validate,session_controller.open)
-router.delete('/',session_controller.close)
     
 module.exports = router;
