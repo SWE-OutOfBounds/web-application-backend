2baf244d6c2c38af645dc62e7e42b776a5298c79
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-05-03 13:34:20 +0200

    ADD: Database script

---
+++ DBINIT.sql
@@ -0,0 +1,25 @@
+DROP DATABASE IF EXISTS PoCBackEnd;
+CREATE DATABASE PoCBackEnd;
+USE PoCBackEnd;
+
+CREATE TABLE users(
+    email VARCHAR(255) PRIMARY KEY,
+    username VARCHAR(255) NOT NULL,
+    firstname VARCHAR(255) NOT NULL,
+    lastname VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE apikeys(
+    secretKey VARCHAR(255) PRIMARY KEY,
+    host VARCHAR(255) NOT NULL,
+    appName VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE blackList(
+    OTT VARCHAR(255) PRIMARY KEY,
+    used DATETIME NOT NULL
+);
+
+INSERT INTO users(email, username, firstname, lastname, password) VALUES('mario.rossi@gmmail.com', 'BigMario', 'Mario', 'Rossi', 'Password1234');
+INSERT INTO apikeys(secretkey, host, appName) VALUES('LQbHd5h334ciuy7', 'http://localhost/', 'ClockCATPCHA-Tester');
