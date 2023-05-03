7df8cdfe3e3a062789dfeffa5c2deb9e60d46e3b
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-05-03 18:55:12 +0200

    FIX : typo

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,6 @@ const router = express.Router();
 
 const clockCaptchaMiddleware = require('../middlewares/clockCaptcha.middleware');
 const user_controller = require("../controllers/user.controller");
-const clockCaptchaMiddleware = require('../middlewares/clockCaptcha.middleware');
 
 /* POST new user */
 router.route("/").post(clockCaptchaMiddleware.validate,user_controller.create);
