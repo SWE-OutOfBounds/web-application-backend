653878fa8031696d70d542cacbc1957eebcf4eb0
jacopo-angeli <jacopo.angeli@studenti.unipd.it>
2023-05-05 05:36:55 +0200

    ADD: swagger folder with configuration and documentation files

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const app = require("./app");
 const port = 3000;
 
+const specs = require('./documentation/swagger');
+
+const swaggerUI = require('swagger-ui-express');
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs));
+
 const server = app.listen(port, () => {
-    console.log(`Api running at http://localhost:${port}`);
-  });
+  console.log(`Api running at http://localhost:${port}`);
+});
